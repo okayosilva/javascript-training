a54e41378d1cb3e8a513302e75730ffafb1fa73f
Kayo <Kayohenriquesilva19@gmail.com>
2024-05-03 16:35:28 -0300

    feat: variaveis e suas funcoes

---
+++ variaveis/script.js
@@ -0,0 +1,46 @@
+var nome = "Kayo";
+var idade = 20;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Silva",
+  cidade = "São Paulo";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefinido);
+
+// console.log(comida);
+// var comida = "Pizza";
+// console.log(comida);
+
+// var time = "Corinthians";
+// console.log(time);
+// time = "Palmeiras";
+// console.log(time);
+
+// Exercícios
+
+let name = "Kayo";
+let age = 20;
+
+let favoriteFood;
+favoriteFood = "Pizza";
+
+let money,
+  car,
+  house,
+  softwareEngineerJob,
+  specialistInJavaScript,
+  favoriteColor,
+  favoriteBook;
