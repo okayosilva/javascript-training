fbc8d0f4b39ec22daf37a4b93814c0d9eeb767e5
Kayo <Kayohenriquesilva19@gmail.com>
2024-05-03 17:24:42 -0300

    feat: tipo de dados

---
+++ tipo-de-dados/script.js
@@ -0,0 +1,16 @@
+let variableTypeString = "Kayo";
+let variableTypeNumber = 20;
+
+let myAge = 20;
+
+let myName = "Kayo";
+let myLastName = "Silva";
+
+let myFullName = `${myName} ${myLastName}`;
+console.log(myFullName);
+
+let phrase = "It's time";
+console.log(phrase);
+
+let verifiedName = myName;
+console.log(typeof verifiedName);
