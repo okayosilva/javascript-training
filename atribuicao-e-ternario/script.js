8158307ede641387a62fb59751d2f4197ea1f11e
Kayo <Kayohenriquesilva19@gmail.com>
2024-05-06 19:35:10 -0300

    feat: atribuicao e ternario

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,12 @@
+let scroll = 1000;
+scroll += 500;
+
+console.log(scroll);
+
+let ownAcar = true,
+  ownHouse = true;
+
+let giveCredit =
+  ownAcar && ownHouse ? "Pode dar crédito" : "Não pode dar crédito";
+
+console.log(giveCredit);
